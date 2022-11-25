dc5d7149b52ac307c6ed95034b43d6573f9f4e3f
Tushar <tushinde1527@gmail.com>
2022-11-25 20:54:56 +0530

    first commit

---
+++ src/main/java/com/WebShoppe/WebShoppeApplication.java
@@ -0,0 +1,13 @@
+package com.WebShoppe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebShoppeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebShoppeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/WebShoppe/WebShoppeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WebShoppe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebShoppeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
